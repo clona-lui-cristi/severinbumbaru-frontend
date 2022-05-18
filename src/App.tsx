c8f6bbaacab1bc055c4ba37d91f58eb68e47233e
w0rd-glitch <panaite.cristian12@gmail.com>
2022-05-18 21:35:22 +0300

    fonts and stuff

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.scss';
 import FacultyList from './components/FacultyList';
 import Formular from './components/Formular';
+import Management from './components/Management';
 import SponsorList from './components/SponsorList';
 import './Responsive.scss';
 
@@ -73,68 +74,7 @@ function App() {
       </div>
 
       <hr></hr>
-      <div className="prof-header">
-        <h1>Management</h1>
-        <h3>They keep the University organized</h3>
-      </div>
-
-      <div className="professors">
-        <div className="professor">
-          <div className="professor-photo"></div>
-          <div className="professor-name">
-            <h3>Prof. univ. dr. ing. Puiu Lucian GEORGESCU</h3>
-          </div>
-          <div className="professor-faculty">
-            <h3>Rector</h3>
-          </div>
-        </div>
-        <div className="professor">
-          <div className="professor-photo"></div>
-          <div className="professor-name">
-            <h3>Prof. univ. dr. Nicoleta Bărbuță - MIȘU</h3>
-          </div>
-          <div className="professor-faculty">
-            <h3>Prorector</h3>
-          </div>
-        </div>
-        <div className="professor">
-          <div className="professor-photo"></div>
-          <div className="professor-name">
-            <h3>Prof. univ. dr. ing. Elena MEREUŢĂ</h3>
-          </div>
-          <div className="professor-faculty">
-            <h3>Prorector</h3>
-          </div>
-        </div>
-        <div className="professor">
-          <div className="professor-photo"></div>
-          <div className="professor-name">
-            <h3>Alexandru NECHIFOR</h3>
-          </div>
-          <div className="professor-faculty">
-            <h3>Prorector</h3>
-          </div>
-        </div>
-        <div className="professor">
-          <div className="professor-photo"></div>
-          <div className="professor-name">
-            <h3>Prof. univ. dr. ec. dr. ing. habil. Silvius STANCIU</h3>
-          </div>
-          <div className="professor-faculty">
-            <h3>Prorector</h3>
-          </div>
-        </div>
-        <div className="professor">
-          <div className="professor-photo"></div>
-          <div className="professor-name">
-            <h3>Conf. univ. dr. Ana ȘTEFĂNESCU</h3>
-          </div>
-          <div className="professor-faculty">
-            <h3>Prorector</h3>
-          </div>
-        </div>
-      </div>
-
+      <Management />
       <FacultyList />
       <SponsorList />
       <Formular />
---
+++ src/components/Management.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+function Management() {
+  return (
+    <div>
+      <div className="prof-header">
+        <h1>Management</h1>
+        <h3>They keep the University organized</h3>
+      </div>
+
+      <div className="professors">
+        <div className="professor">
+          <div className="professor-photo"></div>
+          <div className="professor-name">
+            <h3>Prof. univ. dr. ing. Puiu Lucian GEORGESCU</h3>
+          </div>
+          <div className="professor-faculty">
+            <h3>Rector</h3>
+          </div>
+        </div>
+        <div className="professor">
+          <div className="professor-photo"></div>
+          <div className="professor-name">
+            <h3>Prof. univ. dr. Nicoleta Bărbuță - MIȘU</h3>
+          </div>
+          <div className="professor-faculty">
+            <h3>Prorector</h3>
+          </div>
+        </div>
+        <div className="professor">
+          <div className="professor-photo"></div>
+          <div className="professor-name">
+            <h3>Prof. univ. dr. ing. Elena MEREUŢĂ</h3>
+          </div>
+          <div className="professor-faculty">
+            <h3>Prorector</h3>
+          </div>
+        </div>
+        <div className="professor">
+          <div className="professor-photo"></div>
+          <div className="professor-name">
+            <h3>Alexandru NECHIFOR</h3>
+          </div>
+          <div className="professor-faculty">
+            <h3>Prorector</h3>
+          </div>
+        </div>
+        <div className="professor">
+          <div className="professor-photo"></div>
+          <div className="professor-name">
+            <h3>Prof. univ. dr. ec. dr. ing. habil. Silvius STANCIU</h3>
+          </div>
+          <div className="professor-faculty">
+            <h3>Prorector</h3>
+          </div>
+        </div>
+        <div className="professor">
+          <div className="professor-photo"></div>
+          <div className="professor-name">
+            <h3>Conf. univ. dr. Ana ȘTEFĂNESCU</h3>
+          </div>
+          <div className="professor-faculty">
+            <h3>Prorector</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Management;
